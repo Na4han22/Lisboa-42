3cab811a0687f9a1254e0e10d25a868968c21447
Myo Naing Thant <mythant@c1r3s2.42lisboa.com>
2025-03-15 17:11:39 +0000

    MNT

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: mythant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 07:48:45 by mythant           #+#    #+#             */
-/*   Updated: 2025/03/13 00:13:55 by mythant          ###   ########.fr       */
+/*   Updated: 2025/03/15 16:51:13 by mythant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -16,25 +16,33 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0')
+	while ((s1[i] && s2[i]) && s1[i] == s2[i])
+	{
 		i++;
+	}
 	return (s1[i] - s2[i]);
 }
 
-void	ft_print_argv(int argc, char **argv)
+void	ft_print_argv(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (argc > 1)
+	while (str[i])
 	{
-		while (argv[1][i])
-		{
-			write (1, &argv[1][i], 1);
-			i++;
-		}
-		write(1, "\n", 1);
+		write (1, &str[i], 1);
+		i++;
 	}
+	write (1, "\n", 1);
+}
+
+void	ft_swap(char *s1, char *s2)
+{
+	int	t;
+
+	t = *s1;
+	*s1 = *s2;
+	*s2 = t;
 }
 
 int	main(int argc, char **argv)
@@ -53,15 +61,15 @@ int	main(int argc, char **argv)
 			{
 				if (ft_strcmp(argv[i], argv[j]) > 0)
 				{
-					t = argv[i];
-					argv[i] = argv[j];
-					argv[j] = t;
+					ft_swap(argv[i], argv[j]);
 				}
 				j++;
 			}
 			i++;
 		}
-		ft_print_argv(argc, argv);
+		i = 1;
+		while (i < argc)
+			ft_print_argv(argv[i++]);
 	}
 	return (0);
 }
